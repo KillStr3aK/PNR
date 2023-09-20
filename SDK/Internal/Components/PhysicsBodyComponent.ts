629169fb1bde4c12fc8c5583c49a9c0ac3394819
Nexd <statser07@gmail.com>
2023-09-20 17:45:18 +0200

    PhysicsBodyComponent implementation

--- SDK/Internal/Components/PhysicsBodyComponent.ts
+++ SDK/Internal/Components/PhysicsBodyComponent.ts
@@ -1,13 +1,16 @@
 import { Entity, ComponentData, IComponent } from "@SDK/Models";
+import { SpriteComponent } from "./SpriteComponent";
 
 export class PhysicsBodyComponent implements IComponent {
-    public Body?: Phaser.Physics.Arcade.Body;
+    public Body: Phaser.Physics.Arcade.Body;
 
     constructor(parent: Entity, data?: ComponentData) {
+        const sprite: Phaser.GameObjects.Sprite = parent.GetComponent(SpriteComponent).Sprite;
+        parent.scene.add.existing(sprite) as Phaser.Physics.Arcade.Sprite;
+        this.Body = sprite.body as Phaser.Physics.Arcade.Body;
     }
 
     Destroy(): void {
         this.Body?.destroy();
-        delete this.Body;
     }
 }
