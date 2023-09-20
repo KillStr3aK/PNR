31952edd1b2f5a1af06d4676355f683c5d392d85
Nexd <statser07@gmail.com>
2023-09-20 14:55:05 +0200

    Resolve SDK imports

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite";
+import path from "path";
 
 export default defineConfig({
-	plugins: [],
-	server: { host: '0.0.0.0', port: 8000 },
-	clearScreen: false,
+    plugins: [],
+    server: { host: "0.0.0.0", port: 8000 },
+    clearScreen: false,
+
+    resolve: {
+        alias: {
+            "@SDK": path.resolve("./SDK")
+        }
+    }
 })
