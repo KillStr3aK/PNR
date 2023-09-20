e56bf7c5498e5bd14b0b99fbd292d6d65747ed9e
Nexd <statser07@gmail.com>
2023-09-20 19:04:27 +0200

    Removed debug from EmptyComponent

--- SDK/Internal/Components/EmptyComponent.ts
+++ SDK/Internal/Components/EmptyComponent.ts
@@ -2,10 +2,8 @@ import { Entity, ComponentData, IComponent } from "@SDK/Models";
 
 export class EmptyComponent implements IComponent {
     constructor(parent: Entity, data?: ComponentData) {
-        console.log(`EmptyComponent is attached to entity "${parent.constructor.name}" ${JSON.stringify(data)}`);
     }
 
     Destroy(): void {
-        console.log("EmptyComponent destroyed");
     }
 }
