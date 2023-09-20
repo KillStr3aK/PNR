06729f55f9a529501cb776a8fb2fc96da90e748b
Nexd <statser07@gmail.com>
2023-09-20 18:21:18 +0200

    PhysicsBodyComponent supports ImageComponent and SpriteComponent

--- SDK/Internal/Components/PhysicsBodyComponent.ts
+++ SDK/Internal/Components/PhysicsBodyComponent.ts
@@ -1,15 +1,41 @@
 import { Entity, ComponentData, IComponent } from "@SDK/Models";
-import { SpriteComponent } from "./SpriteComponent";
+import { SpriteComponent, ImageComponent } from "@SDK/Internal";
+
+export enum PhysicsBodyType {
+    NONE,
+    SPRITE,
+    IMAGE
+};
 
 export class PhysicsBodyComponent implements IComponent {
     public Body: Phaser.Physics.Arcade.Body;
+    public BodyType: PhysicsBodyType;
 
     constructor(parent: Entity, data?: ComponentData) {
-        const sprite: Phaser.GameObjects.Sprite = parent.GetComponent(SpriteComponent).Sprite;
-        parent.scene.add.existing(sprite) as Phaser.Physics.Arcade.Sprite;
-        this.Body = sprite.body as Phaser.Physics.Arcade.Body;
+        this.BodyType = parent.HasComponent(SpriteComponent)
+            ? PhysicsBodyType.SPRITE : parent.HasComponent(ImageComponent) ? PhysicsBodyType.IMAGE : PhysicsBodyType.NONE;
 
-        this.Body.setSize(sprite.width, sprite.height);
+        let object: Phaser.GameObjects.Image | Phaser.GameObjects.Sprite;
+
+        switch (this.BodyType)
+        {
+            case PhysicsBodyType.IMAGE:
+            {
+                object = parent.GetComponent(ImageComponent).Image;
+                parent.scene.physics.add.existing(object) as Phaser.Physics.Arcade.Image;
+            } break;
+
+            case PhysicsBodyType.SPRITE:
+            {
+                object = parent.GetComponent(SpriteComponent).Sprite
+                parent.scene.physics.add.existing(object) as Phaser.Physics.Arcade.Sprite;
+            } break;
+
+            case PhysicsBodyType.NONE: throw new Error(`PhysicsBodyComponent on parent entity ${parent.GetType()} (${parent.GetInstance()}) requires SpriteComponent or ImageComponent`);
+        }
+
+        this.Body = object.body as Phaser.Physics.Arcade.Body;
+        this.Body.setSize(object.width, object.height);
         this.Body.updateBounds();
 
         if (data?.MaxVelocityX) {
