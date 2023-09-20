2e85b1c7dd41efe3aefa63686dcc3c150962e1bf
Nexd <statser07@gmail.com>
2023-09-20 14:54:58 +0200

    Print SDK Version on initialization

--- src/models/Game.ts
+++ src/models/Game.ts
@@ -1,8 +1,9 @@
+import { SDKVersion } from "@SDK/Enums";
 import { BaseGame } from "@SDK/Internal";
 
 export class Game extends BaseGame {
     constructor(config: Phaser.Types.Core.GameConfig) {
         super(config);
-        console.info(`Initializing game instance ${config.title}`);
+        console.info(`Initializing game instance ${config.title} with SDK ${SDKVersion.MAJOR}.${SDKVersion.MINOR}.${SDKVersion.PATCH}.${SDKVersion.REVISION}`);
     }
 }
