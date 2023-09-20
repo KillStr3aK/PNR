d3f25f57e1c7c64bdedbc79aae628414e5fdb5fd
Nexd <statser07@gmail.com>
2023-09-20 16:46:08 +0200

    Prefabs may have name

--- SDK/Models/Prefab/Prefab.ts
+++ SDK/Models/Prefab/Prefab.ts
@@ -1,5 +1,6 @@
 import { PrefabComponent, IComponentConstructor } from "@SDK/Models";
 
 export type Prefab = {
+    Name: string | symbol;
     Components: (PrefabComponent | IComponentConstructor)[];
 }
