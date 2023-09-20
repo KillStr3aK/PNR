1294d61770f25f7c4f8f218fa9ab6b1549c8e628
Nexd <statser07@gmail.com>
2023-09-20 15:45:21 +0200

    Arbitrary data for EmptyComponent

--- SDK/Internal/Prefabs/WorldPrefab.ts
+++ SDK/Internal/Prefabs/WorldPrefab.ts
@@ -4,7 +4,10 @@ import { EmptyComponent } from "../Components/EmptyComponent";
 export const WorldPrefab: Prefab = {
     Components: [
         {
-            Component: EmptyComponent
+            Component: EmptyComponent,
+            Data: {
+                Key: "Value"
+            }
         }
     ]
 };
