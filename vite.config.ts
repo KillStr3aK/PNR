f508b67082e1eff56816c4faf4e09219bc524aef
Nexd <statser07@gmail.com>
2023-09-20 14:55:46 +0200

    Codestyle

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,14 @@
-import { defineConfig } from "vite";
-import path from "path";
+import { defineConfig as DefineConfig} from "vite";
+import Path from "path";
 
-export default defineConfig({
+export default DefineConfig({
     plugins: [],
     server: { host: "0.0.0.0", port: 8000 },
     clearScreen: false,
 
     resolve: {
         alias: {
-            "@SDK": path.resolve("./SDK")
+            "@SDK": Path.resolve("./SDK")
         }
     }
 })
