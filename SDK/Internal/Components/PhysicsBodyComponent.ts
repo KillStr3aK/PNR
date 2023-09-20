cfe1735cbd732d4f17f792dce1347b7d8721ca76
Nexd <statser07@gmail.com>
2023-09-20 17:50:53 +0200

    Rest of the implementation

--- SDK/Internal/Components/PhysicsBodyComponent.ts
+++ SDK/Internal/Components/PhysicsBodyComponent.ts
@@ -8,6 +8,17 @@ export class PhysicsBodyComponent implements IComponent {
         const sprite: Phaser.GameObjects.Sprite = parent.GetComponent(SpriteComponent).Sprite;
         parent.scene.add.existing(sprite) as Phaser.Physics.Arcade.Sprite;
         this.Body = sprite.body as Phaser.Physics.Arcade.Body;
+
+        this.Body.setSize(sprite.width, sprite.height);
+        this.Body.updateBounds();
+
+        if (data?.MaxVelocityX) {
+            this.Body.maxVelocity.x = data.MaxVelocityX;
+        }
+
+        if (data?.CollideWithWorldBounds) {
+            this.Body.collideWorldBounds = true;
+        }
     }
 
     Destroy(): void {
