48b04eff62f6a4d1b46a93469bd3a4f700996219
Nexd <statser07@gmail.com>
2023-09-20 17:45:07 +0200

    SpriteComponent implementation

--- SDK/Internal/Components/SpriteComponent.ts
+++ SDK/Internal/Components/SpriteComponent.ts
@@ -1,13 +1,22 @@
 import { Entity, ComponentData, IComponent } from "@SDK/Models";
 
 export class SpriteComponent implements IComponent {
-    public Sprite?: Phaser.GameObjects.Sprite;
+    public Sprite: Phaser.GameObjects.Sprite;
 
-    constructor(parent: Entity, data?: ComponentData) {
+    constructor(parent: Entity, data: ComponentData) {
+        this.Sprite = parent.scene.add.sprite(data.X, data.Y, data.Texture, data.Frame);
+        this.Sprite.y -= this.Sprite.height * this.Sprite.originY;
+
+        if (data.Scale) {
+            this.Sprite.setScale(data.Scale);
+        }
+
+        if (data.Depth) {
+            this.Sprite.setDepth(data.Depth);
+        }
     }
 
     Destroy(): void {
-        this.Sprite?.destroy();
-        delete this.Sprite;
+        this.Sprite.destroy();
     }
 }
