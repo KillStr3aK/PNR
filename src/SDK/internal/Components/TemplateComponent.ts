d4146e11bfadebd02f9a037044774b1c9b9d2f70
Nexd <statser07@gmail.com>
2023-09-20 13:44:43 +0200

    Template component

---
+++ src/SDK/internal/Components/TemplateComponent.ts
@@ -0,0 +1,9 @@
+import { ComponentData, IComponent, IComponentable } from "../../models";
+
+export class TemplateComponent implements IComponent {
+    constructor() {
+    }
+
+    Destroy(): void {
+    }
+}
