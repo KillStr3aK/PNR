a35226bca56d69c08c8cb9c9a29173a109a7e928
Nexd <statser07@gmail.com>
2023-09-20 16:46:50 +0200

    Prefab update

--- SDK/Internal/Prefabs/WorldPrefab.ts
+++ SDK/Internal/Prefabs/WorldPrefab.ts
@@ -2,6 +2,7 @@ import { Prefab } from "@SDK/Models";
 import { EmptyComponent } from "@SDK/Internal";
 
 export const WorldPrefab: Prefab = {
+    Name: "world_prefab",
     Components: [
         {
             Component: EmptyComponent,
