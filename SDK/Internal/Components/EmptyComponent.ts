ffc3290adaa48cee590c1a1b704e0eb5429bab10
Nexd <statser07@gmail.com>
2023-09-21 05:08:37 +0200

    Adjusted codestyle

--- SDK/Internal/Components/EmptyComponent.ts
+++ SDK/Internal/Components/EmptyComponent.ts
@@ -1,9 +1,6 @@
 import { Entity, ComponentData, IComponent } from "@SDK/Models";
 
 export class EmptyComponent implements IComponent {
-    constructor(parent: Entity, data?: ComponentData) {
-    }
-
-    Destroy(): void {
-    }
+    constructor(parent: Entity, data?: ComponentData) { }
+    Destroy(): void { }
 }
