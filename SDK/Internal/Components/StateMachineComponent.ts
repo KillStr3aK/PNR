951175cdb51eeb05aeddcde05166edf4fa56b3f5
Nexd <statser07@gmail.com>
2023-09-22 01:11:27 +0200

    Finish up implementation

--- SDK/Internal/Components/StateMachineComponent.ts
+++ SDK/Internal/Components/StateMachineComponent.ts
@@ -1,18 +1,21 @@
 import { Entity, ComponentData, IComponent, StateMachine, State, TransitionType } from "@SDK/Models";
+import { StateSets } from "@SDK/Internal";
 
 export class StateMachineComponent implements IComponent {
     public StateMachine: StateMachine<Entity>;
 
     constructor(parent: Entity, data: ComponentData) {
-        // TODO PARAMS
-        this.StateMachine = new StateMachine<Entity>(parent.GetScene(), parent, [], {} as State<Entity>);
+        const states = StateSets.GetSet(data.StateSet);
+        const initialState = StateSets.GetValue<State<Entity>>(data.InitialStateId);
+
+        this.StateMachine = new StateMachine<Entity>(parent.GetScene(), parent, states, initialState);
 
         if (data.Disabled) {
             this.StateMachine.Disable();
         } else {
             this.StateMachine.DoTransition({
                 Type: TransitionType.INITIAL,
-                To: {} as any
+                To: initialState.ID
             });
         }
     }
