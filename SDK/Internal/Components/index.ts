e67a45f7b11b5127d4e6e5c53f9d5d9bed7c7f7c
Nexd <statser07@gmail.com>
2023-09-21 23:29:26 +0200

    Added missing exports to indexer

--- SDK/Internal/Components/index.ts
+++ SDK/Internal/Components/index.ts
@@ -2,3 +2,5 @@ export * from "./EmptyComponent";
 export * from "./PhysicsBodyComponent";
 export * from "./SpriteComponent";
 export * from "./ImageComponent";
+export * from "./SceneComponent";
+export * from "./StateMachineComponent";
