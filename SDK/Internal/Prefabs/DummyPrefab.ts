0d56be4fe8edc816f486c1a82cccf579fb92a5cf
Nexd <statser07@gmail.com>
2023-09-22 03:01:19 +0200

    Changed ImageComponent to SpriteComponent and added ColliderComponent

--- SDK/Internal/Prefabs/DummyPrefab.ts
+++ SDK/Internal/Prefabs/DummyPrefab.ts
@@ -1,11 +1,12 @@
-import { Prefab } from "@SDK/Models";
-import { PhysicsBodyComponent, ImageComponent } from "@SDK/Internal";
+import { Entity, Prefab } from "@SDK/Models";
+import { EntityType } from "@SDK/Enums";
+import { PhysicsBodyComponent, ColliderComponent, ColliderObjectType, BaseScene, SpriteComponent } from "@SDK/Internal";
 
 export const DummyPrefab: Prefab = {
     Name: "dummy_prefab",
     Components: [
         {
-            Component: ImageComponent,
+            Component: SpriteComponent,
             Data: {
                 X: 400,
                 Y: 100,
@@ -18,6 +19,26 @@ export const DummyPrefab: Prefab = {
             Component: PhysicsBodyComponent,
             Data: {
                 CollideWithWorldBounds: true
+            }
+        },
+
+        {
+            Component: ColliderComponent,
+            Data: {
+                Objects: (scene: BaseScene): ColliderObjectType => {
+                    const entities: Entity[] = scene.Internal.Entities.GetEntities(EntityType.DUMMY);
+                    const sprites: Phaser.GameObjects.Sprite[] = [];
+
+                    entities.forEach(entity => {
+                        console.log(entity.body);
+                        sprites.push(entity.GetComponent<typeof SpriteComponent>(SpriteComponent).Sprite);
+                    });
+
+                    return sprites;
+                },
+
+                OnCollide: () => { console.log("collide"); },
+                OnProcess: () => { console.log("process"); return true; }
             }
         }
     ]
